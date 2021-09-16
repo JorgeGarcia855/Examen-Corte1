88e7a43f16ee4b54c3ce64662f34b225b8402a41
JorgeGarcia855 <jlgarciap@unbosque.edu.co>
2021-09-16 16:49:41 -0500

    Se muestra los datos en una tabla

--- js/main.js
+++ js/main.js
@@ -7,30 +7,40 @@ $(document).ready(function() {
       dataType: 'json',
       success: function(data) {
         let speechInd = 0, defInd = 0, exInd = 0;
+        let speechNum = 1, defNum = 1, exNum = 1;
+
         let word = data[0].word;
         let phonetic = data[0].phonetic;
         let audio = data[0].phonetics[0].audio;
         let origin = data[0].origin;
-        let meanings = data[0].meanings
+        let meanings = data[0].meanings;
 
-        for (let obj in meanings) {
+        $('#content').find('tbody').append('<tr>'
+        +'<td>'+word+'</td>'
+        +'<td>'+phonetic+'</td>'
+        +'<td>'+audio+'</td>'
+        +'<td>'+origin+'</td>'+
+        '</tr>');
+
+        for (obj in meanings) {
           let partOfSpeech = meanings[speechInd].partOfSpeech
-          console.log(partOfSpeech)
-          speechInd++
-        }
-
-        for (let obj in meanings) {
           let definition = meanings[defInd].definitions[0].definition
-          console.log(definition)
-          defInd++
-        }
-        
-        for (let obj in meanings) {
           let example = meanings[exInd].definitions[0].example
-          console.log(example)
-          exInd++
+
+          $('#meanings').find('tbody').append('<tr>'
+          +'<td>'+word+'</td>'
+          +'<td>'+speechNum+'. '+partOfSpeech+'</td>'
+          +'<td>'+defNum+'. '+definition+'</td>'
+          +'<td>'+exNum+'. '+example+'</td>'+
+          '</tr>')
+
+          speechInd++;
+          speechNum++;
+          defInd++;
+          defNum++;
+          exInd++;
+          exNum++;
         }
-        
       }
     });
   });
